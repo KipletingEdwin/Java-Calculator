f9dcbed005ecc1181ccf4b15ff38612cd2dc125b
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-01 16:32:53 +0300

    add Java GUI

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class Main implements ActionListener{
+
+public class Main implements ActionListener {
     JFrame frame;
     JTextField textfield;
     JButton[] numberButtons = new JButton[10];
@@ -11,19 +11,24 @@ public class Main implements ActionListener{
     JButton decButton, equButton, delButton, clrButton;
     JPanel panel;
 
-    Font myFont = new Font("Ink Free",Font.BOLD,30);
-    double num1=0,num2=0,result=0;
-    char operator
-    Calculator(){
+    Font myFont = new Font("Ink Free", Font.BOLD, 30);
+    double num1 = 0, num2 = 0, result = 0;
+    char operator;
 
+    Main() {
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
+
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
+        Main calc = new Main();
+    }
 
-        }
-
-    @java.lang.Override
-    public void actionPerformed(java.awt.event.ActionEvent e) {
-
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Implement action listener functionality here
     }
 }
